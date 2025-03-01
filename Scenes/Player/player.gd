1db9bafa5df51ed966b1be0649486f079190e89d
VenturingForth <joshua@codeadventure.uk>
2025-03-01 21:31:26 +0000

    added exported variables to player scene script

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,17 +1,19 @@
 extends CharacterBody2D
 
+@export var hp: int = 10
+
+@export var move_speed: float = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var score = 200
 	
-	print(score)
+	print(hp)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var move_speed: float = 100
+	
 	velocity = move_vector * move_speed
 	
 	move_and_slide()
