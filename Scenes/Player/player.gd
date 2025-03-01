b07140717438a8d47ef99668a8d05c6d91974b9a
VenturingForth <joshua@codeadventure.uk>
2025-03-01 21:24:46 +0000

    project setup

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var score = 200
+	
+	print(score)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var move_speed: float = 100
+	velocity = move_vector * move_speed
+	
+	move_and_slide()
