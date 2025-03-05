9f7b59ded7d03fb1a6f510b7e04d30d63aa35f88
VenturingForth <joshua@codeadventure.uk>
2025-03-05 22:16:09 +0000

    added block movement physics

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 class_name Player
 
 @export var move_speed: float = 100
+@export var push_strength: float = 140
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -29,5 +30,17 @@ func _process(delta: float) -> void:
 	
 	else:
 		$AnimatedSprite2D.stop() 
+	
+	# Get the last collision
+	# Check if it's the block we're colliding with
+	# If it's the block, push it
+	
+	var collision: KinematicCollision2D = get_last_slide_collision()
+	if collision:
+		
+		var collider_node = collision.get_collider()
+		if collider_node is RigidBody2D:
+			var collision_normal: Vector2 = collision.get_normal()
+			collider_node.apply_central_force(-collision_normal * push_strength)
 	
 	move_and_slide()
