1348beb20b363ea80a52638d3547787d3c1c00c3
VenturingForth <joshua@codeadventure.uk>
2025-03-05 22:32:01 +0000

    changed _process to _physics process in player script to avoid fps issues

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	velocity = move_vector * move_speed
