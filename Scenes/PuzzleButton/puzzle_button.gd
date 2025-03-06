ed5b87b8aaae397e154edec02443da5f66b0b2ca
VenturingForth <joshua@codeadventure.uk>
2025-03-06 20:36:31 +0000

    created pushable button

---
+++ Scenes/PuzzleButton/puzzle_button.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+var bodies_on_top: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	bodies_on_top = true
+	if body.is_in_group("pushable") or body is Player:
+		if bodies_on_top:
+			$AnimatedSprite2D.play("pressed")
+
+func _on_body_exited(body: Node2D) -> void:
+	bodies_on_top = false
+	if body.is_in_group("pushable") or body is Player:
+		if !bodies_on_top:
+			$AnimatedSprite2D.play("unpressed")
