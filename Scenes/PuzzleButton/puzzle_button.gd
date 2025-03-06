29c5c57190de039f80b3a1a94b7f3a472513edf2
VenturingForth <joshua@codeadventure.uk>
2025-03-06 21:47:34 +0000

    added inherited scenes for ice block and single press button

--- Scenes/PuzzleButton/puzzle_button.gd
+++ Scenes/PuzzleButton/puzzle_button.gd
@@ -1,18 +1,10 @@
 extends Area2D
 
+@export var is_single_use = false
+
 var bodies_on_top: bool = false
 signal pressed
 signal unpressed
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _on_body_entered(body: Node2D) -> void:
 	bodies_on_top = true
@@ -22,6 +14,9 @@ func _on_body_entered(body: Node2D) -> void:
 			$AnimatedSprite2D.play("pressed")
 
 func _on_body_exited(body: Node2D) -> void:
+	if is_single_use:
+		return
+	
 	bodies_on_top = false
 	if body.is_in_group("pushable") or body is Player:
 		if !bodies_on_top:
