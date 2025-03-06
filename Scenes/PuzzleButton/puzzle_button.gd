22a2e28535f8e002b5e2f5821f088c3a45d03675
VenturingForth <joshua@codeadventure.uk>
2025-03-06 20:56:35 +0000

    added locked dungeon door

--- Scenes/PuzzleButton/puzzle_button.gd
+++ Scenes/PuzzleButton/puzzle_button.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 var bodies_on_top: bool = false
+signal pressed
+signal unpressed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,10 +18,12 @@ func _on_body_entered(body: Node2D) -> void:
 	bodies_on_top = true
 	if body.is_in_group("pushable") or body is Player:
 		if bodies_on_top:
+			pressed.emit()
 			$AnimatedSprite2D.play("pressed")
 
 func _on_body_exited(body: Node2D) -> void:
 	bodies_on_top = false
 	if body.is_in_group("pushable") or body is Player:
 		if !bodies_on_top:
+			unpressed.emit()
 			$AnimatedSprite2D.play("unpressed")
