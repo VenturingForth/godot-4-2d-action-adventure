8b034f4821d94f9c6cb89dc62d909efc13932c14
VenturingForth <joshua@codeadventure.uk>
2025-03-09 11:14:47 +0000

    added npc_fighter scene and interact action

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -13,6 +13,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	
+	move_player()
+	push_blocks()
+	move_and_slide()
+
+func move_player():
 	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	velocity = move_vector * move_speed
@@ -32,16 +38,10 @@ func _physics_process(delta: float) -> void:
 	else:
 		$AnimatedSprite2D.stop() 
 	
-	# Get the last collision
-	# Check if it's the block we're colliding with
-	# If it's the block, push it
-	
+func push_blocks():
 	var collision: KinematicCollision2D = get_last_slide_collision()
 	if collision:
-		
 		var collider_node = collision.get_collider()
 		if collider_node.is_in_group("pushable"):
 			var collision_normal: Vector2 = collision.get_normal()
 			collider_node.apply_central_force(-collision_normal * push_strength)
-	
-	move_and_slide()
